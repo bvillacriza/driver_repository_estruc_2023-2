26abd39a24165fdba7fc5070390e8c79c6936c43
bvillacriza <bvillacriza@unal.edu.co>
2023-10-30 14:54:45 -0500

    feat(ring buffer): adding get and size APIs to rng buffer library.

--- Driver_repository/Core/Src/main.c
+++ Driver_repository/Core/Src/main.c
@@ -61,6 +61,22 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int _write(int file, char *ptr, int len)
+{
+  HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+  return len;
+}
+/**
+  * @brief  Rx Transfer completed callback.
+  * @param  huart UART handle.
+  * @retval None
+  */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	ring_buffer_put(&ring_buffer_uart_rx, rx_data);
+
+	HAL_UART_Receive_IT(&huart2, &rx_data, 1);
+}
 
 /* USER CODE END 0 */
 
@@ -94,7 +110,9 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+  ring_buffer_init(&ring_buffer_uart_rx, rx_buffer, 16);
 
+   HAL_UART_Receive_IT(&huart2, &rx_data, 1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -102,7 +120,16 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  uint16_t size = ring_buffer_size(&ring_buffer_uart_rx);
+	  	  if (size != 0) {
+	  		  uint8_t rx_message[size + 1];
+	  		  for (uint16_t idx = 0; idx < size; idx++) {
+	  			  ring_buffer_get(&ring_buffer_uart_rx, &rx_message[idx]);
+	  		  }
+	  		  rx_message[size] = 0;
+	  		  printf("Rec: %s\r\n", rx_message);
+	  	  }
+	  	  HAL_Delay(1000); // to wait one second
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
--- Driver_repository/Core/Src/ring_buffer.c
+++ Driver_repository/Core/Src/ring_buffer.c
@@ -25,3 +25,47 @@ void ring_buffer_init(ring_buffer_t *ring_buffer, uint8_t *buffer, uint16_t capa
  * @brief This function adds a data in the ring buffer
  * @param data: the value to be added
 */
+uint8_t ring_buffer_put(ring_buffer_t *ring_buffer, uint8_t data)
+{
+	ring_buffer->buffer[ring_buffer->head] = data;
+	ring_buffer->head = (ring_buffer->head + 1) % ring_buffer->capacity;
+
+	if (ring_buffer->is_full != 0) {
+		ring_buffer->tail = (ring_buffer->tail + 1) % ring_buffer->capacity;
+	}
+
+	if (ring_buffer->head == ring_buffer->tail) {
+		ring_buffer->is_full = 1;
+	}
+}
+
+/**
+ * @brief This function reads a data from the ring buffer
+ * @param data: the pointer to the address to write the data
+*/
+uint8_t ring_buffer_get(ring_buffer_t *ring_buffer, uint8_t *data)
+{
+	if ((ring_buffer->is_full != 0) || (ring_buffer->tail != ring_buffer->head)) {
+		*data = ring_buffer->buffer[ring_buffer->tail];
+		ring_buffer->tail = (ring_buffer->tail + 1) % ring_buffer->capacity;
+		ring_buffer->is_full = 0;
+		return 1;
+	}
+	return 0;
+}
+
+/**
+ * @brief This function returns the size of the ring buffer
+*/
+uint16_t ring_buffer_size(ring_buffer_t *ring_buffer)
+{
+	uint16_t size = 0;
+
+	if ((ring_buffer->is_full == 0) && (ring_buffer->tail <= ring_buffer->head)) {
+		size = ring_buffer->head - ring_buffer->tail;
+	} else {
+		size = ring_buffer->head + (ring_buffer->capacity - ring_buffer->tail);
+	}
+
+	return size;
+}
