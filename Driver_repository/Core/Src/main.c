878ae54fd5f3a68702f3d81c39fed8c3a460a542
bvillacriza <bvillacriza@unal.edu.co>
2023-11-08 12:29:19 -0500

    feat(display): adding I2C initialization.

--- Driver_repository/Core/Src/main.c
+++ Driver_repository/Core/Src/main.c
@@ -42,6 +42,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -56,6 +58,7 @@ uint8_t rx_data;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -79,9 +82,13 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
 	// Coloca el dato Rx en un buffer circular y configura la recepción nuevamente
 
-	ring_buffer_put(&ring_buffer_uart_rx, rx_data);
 
-	HAL_UART_Receive_IT(&huart2, &rx_data, 1);
+		if (ring_buffer_put(&ring_buffer_uart_rx, rx_data) == 0) {
+			printf("Rx buffer is full\r\n");
+		}
+
+		HAL_UART_Receive_IT(&huart2, &rx_data, 1);
+
 }
 
 
@@ -116,6 +123,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
   ring_buffer_init(&ring_buffer_uart_rx, rx_buffer, 16);
 
@@ -128,25 +136,15 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	  uint16_t size = ring_buffer_size(&ring_buffer_uart_rx);
-	  	  if (size != 0) {
-	  		  uint8_t rx_message[size + 1];
-	  		  for (uint16_t idx = 0; idx < size; idx++) {
-	  			  ring_buffer_get(&ring_buffer_uart_rx, &rx_message[idx]);
-	  		  }
-	  		  rx_message[size] = 0;
-	  		  printf("Rec: %s\r\n", rx_message);
-	  	  }
-	  	  HAL_Delay(1000); // to wait one second
-
-	  	/*uint8_t is_empty = ring_buffer_is_empty(&ring_buffer_uart_rx); */
-
-	  	// Check if the ring buffer is empty
-	  	if (ring_buffer_is_empty(&ring_buffer_uart_rx)) {
-	  	  // The ring buffer is empty
-	  	} else {
-	  	  // The ring buffer is not empty
-	  	}
-
+	  if (size != 0) {
+		  uint8_t rx_message[size + 1];
+		  for (uint16_t idx = 0; idx < size; idx++) {
+			  ring_buffer_get(&ring_buffer_uart_rx, &rx_message[idx]);
+		  }
+		  rx_message[size] = 0;
+		  printf("Rec: %s\r\n", rx_message);
+	  }
+	  HAL_Delay(1000); // to wait one second
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -199,6 +197,54 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x10909CEC;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
